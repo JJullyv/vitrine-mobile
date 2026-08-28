836080c2b5bfe48ca0b8406554ba2249c264254a
Jully Vasconcelos <jullypsi@gmail.com>
2026-08-28 11:59:58 -0300

    aula1 projeto expo com TypeScript rodando

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.titulo}>Vitrine</Text>
+      <Text style={styles.subtitulo}>
+        Jully - Dispositivos Móveis 2026
+      </Text>
       <StatusBar style="auto" />
-    </View>
+     </View> 
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#350f0f',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: { color: '#61DAFB', fontSize: 34, fontWeight: 'bold' },
+  subtitulo: { color: '#FFFFFF', fontSize: 15, marginTop: 8 },
+
 });
