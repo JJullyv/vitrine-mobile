874098b66669113f0175a50fa4f918cb9478c1a7
Jully Vasconcelos <jullypsi@gmail.com>
2026-08-28 12:50:04 -0300

    duranteaula2
    Criando um arquivo de constantes (top6)

--- App.tsx
+++ App.tsx
@@ -1,28 +1,23 @@
-
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Vitrine</Text>
-      <Text style={styles.subtitulo}>
-        Jully - Dispositivos Móveis 2026
-      </Text>
+      <Text style={styles.subtitulo}>Jully - Dispositivos Móveis 2026</Text>
       <StatusBar style="auto" />
-     </View> 
+    </View>
   );
-
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#350f0f',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#350f0f",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  titulo: { color: '#61DAFB', fontSize: 34, fontWeight: 'bold' },
-  subtitulo: { color: '#FFFFFF', fontSize: 15, marginTop: 8 },
-
+  titulo: { color: "#61DAFB", fontSize: 34, fontWeight: "bold" },
+  subtitulo: { color: "#FFFFFF", fontSize: 15, marginTop: 8 },
 });
